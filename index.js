8c2ec68e25d7791f24d819fcb57cd53033c1ec70
VikasNimap <vikasmaurya@nimapinfotech.com>
2022-12-17 10:06:23 +0530

    done

---
+++ index.js
@@ -0,0 +1,77 @@
+// console.log("hello");
+// let a = "sVikas";
+// let res = a.charAt(0);
+// let out = a.charCodeAt(1);
+// let res = a.codePointAt(0);
+// let res = a.concat("HII")
+// let res = a.endsWith('s')
+// let res = a.includes('i');
+// let res = a.indexOf('i');
+// let res = a.lastIndexOf('s')
+// let res = a.match("Vi")
+// let res = a.matchAll(/'sVikas'/g)
+// let res = a.repeat(2)
+// let res  = a.split(',')
+// let res = a.
+// console.log(res);
+// let data = [34,56,3];
+// let res = data.map((e)=>{
+//     return e * 2
+// })
+// console.log(res);
+// let data = [12,34,56,2];
+// let res = data.filter((e)=>{
+//     return e>5
+// })
+// console.log(res);
+
+// let data = [3,5,6,7];
+// let res = data.reduce((acc,curr)=>{
+//     acc = curr+acc
+//     return acc
+// })
+// console.log(res);
+let data = ["vikas","hii","vi"];
+let res = data.filter((e)=>{
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
