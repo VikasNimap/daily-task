f5778a5337a565336648bbf2a3f9111a1b31176f
VikasNimap <118968201+VikasNimap@users.noreply.github.com>
2022-12-19 17:49:59 +0530

    Add files via upload

---
+++ 19_dec/js/index.js
@@ -0,0 +1,65 @@
+// console.log("hello");
+// let data = [1,23,44,111,10,8,5];
+// for(let i = 0;i<data.length;i++){                   //1                    i [1,]
+//     for(let j = 0;j<i;j++){                         //0  0<1               j [23,]
+//         if(data[i]<data[j]){                        // 23 44
+//             let temp = data[i]                      // t = 23
+//             data[i]=data[j]                         // i = 44
+//             data[j]=temp                            // 23 = t
+//         }
+//     }
+// }
+// console.log(data[i]);
+
+// let rows = 5;
+// let str=""
+// for (let i = 1; i <= rows; i++) {
+//     for (let j = 1; j >= 5; j++) {
+//         str+="*"
+//     }
+//     str+="\n"
+// }
+// // console.log(str);
+// // for (let i = rows; i >= 1; i--) {
+// //     for (let j = 1; j <= i; j++) {
+// //         str+="* "
+// //     }
+// //     str+="\n"
+// // }
+// console.log(str);
+// console.log(str);
+
+
+// let str = "";
+// for (let i = 1; i <= 5; i++) {
+//     for (let j = 1;j<=i;j++){
+//         str+="* "
+//     }
+//     str+="\n"
+// }
+// for (let i = 4; i >=1; i--) {
+//     for (let j = 1;j<=i;j++){
+//         str+="* "
+//     }
+//     str+="\n"
+// }
+// console.log(str);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
