f47920024b38d1b2a7d17d0c4e99e2c39c99d777
VikasNimap <118968201+VikasNimap@users.noreply.github.com>
2023-02-03 17:59:14 +0530

    Add files via upload

---
+++ 03_feb/js/index.js
@@ -0,0 +1,58 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Welcome to Programiz!");
+// let firstNum = 0;
+// let secondNum = 1;
+// let arr = [];
+// arr.push(firstNum,secondNum);
+// for(let i = 0;i<=10-2;i++){
+// let nextNum = firstNum + secondNum;
+// arr.push(nextNum);
+// firstNum = secondNum
+// secondNum = nextNum
+// }
+// console.log(arr);
+
+// function fibonacci(num){
+//     if(num==0){
+//         return 0
+//     }
+//     else if (num>0){
+//         let firstNum = 0
+//         let secondNum = 1
+//     let nextNum = fibonacci(firstNum) + fibonacci(second)
+//     return nextNum
+//     }
+// }
+
+// console.log(fibonacci(10))
+
+// let str = "vikas";
+// let str2 = "apvki";
+// let s1 = str.split("").sort();
+// let s2 = str2.split("").sort();
+// let s11 = s1.join("");
+// let s12 = s2.join("");
+// if(s11===s12){
+//     console.log(`given strings are anagram.`)
+// }
+// else{
+//     console.log(`given strings are not anagram`);
+// }
+
+// let str1 = "vikai";
+// let Objstr1 = {};
+// for(let i of str1){
+//     Objstr1[i] = (Objstr1[i] || 0) + 1
+// }
+// console.log(Objstr1);
+
+// let num = 345;
+// let res = "";
+// while(num>0){
+//     res +=  num % 10
+//     // res += num
+//     num = Math.floor(num/10)
+// }
+// console.log(Number(res))
