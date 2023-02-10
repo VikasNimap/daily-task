da266896045b5134c6ced1f8269b36fcf17893a1
VikasNimap <118968201+VikasNimap@users.noreply.github.com>
2023-02-10 17:45:45 +0530

    Add files via upload

---
+++ 10_feb/js/index.js
@@ -0,0 +1,184 @@
+// console.log("vikas");
+// let data = [
+//     {
+//         name: "Vikas",
+//         age: 20,
+//         phone: "34354567"
+//     },
+//     {
+//         name: "Akash",
+//         age: 20,
+//         phone: "34354567"
+//     },
+//     {
+//         name: "Satendra",
+//         age: 20,
+//         phone: "34354567"
+//     },
+//     {
+//         name: "Virendra",
+//         age: 20,
+//         phone: "34354567"
+//     },
+// ]
+// let arrMap = data.map((e) => {
+//     const a = e["name"];
+//     console.log(a);
+// })
+// let data = [
+//     {
+//         mainCategoryId: "2392849048204824"
+//     },
+//     {
+//         mainCategoryId: "2392849048204824"
+//     },
+//     {
+//         mainCategoryId: "2392849048204824"
+//     },
+//     {
+//         mainCategoryId: "2392849048204824"
+//     },
+//     {
+//         mainCategoryId: "2392849048204824"
+//     },
+// ]
+// const companies = await Comapnies.find({})
+
+
+// let num = 20;
+// let arr = [];
+// for(let i = 0;i<=num;i++){
+//     if(i%2==0){
+//         arr.push(i)
+//     }
+// }console.log(arr);
+// let data = [23,34,67,1,90,1];
+// let arr = [];
+// for(let i = 0;i<data.length;i++){
+//     for(let j = 0;j<=i;j++){
+//         if(data[i] != data[j]){
+//             arr.push(data[j])
+//         }
+//     }
+// }console.log(arr);
+// let num = 100;
+// let arr = [];
+// let count = 0;
+// for(let i = 2;i<=100;i++){
+//     count = 0;
+//     for(let j = 2;j<=i;j++){
+//         if(i%j==0){
+//             count++
+//         }
+//     }
+//     if(count==1){
+//         arr.push(i)
+//     }
+// }
+// console.log(arr);
+// let data = 100;
+// let out = []
+// for(let i = 100; i>=0;i--){
+//     // console.log(i);
+//     out.push(i)
+// }
+// console.log(out);
+
+// let data = [23,56,89,12,23];
+// let arr = [];
+// for(let i = data.length-1;i>=0;i--){
+//     arr.push(data[i])
+// }
+// console.log(arr);
+
+
+// let data = [12, 34, 56, 12, 67, 34];
+// let arr = [];
+// for (let i = 0; i < data.length; i++) {
+//     for (let j = 0; j < i; j++) {
+//         if (data[i] == data[j]) {
+//             data.pop(data[i])
+
+//         }
+//     }
+// } console.log(data);
+
+// let str = " v i k a x               ";
+// let str1 = str.trim().trimEnd();
+// console.log(str1);
+
+// let data = [12,34,67,90,12];
+// for(let i = 0;i<data.length;i++){
+//     for(let j = 0;j<)
+// }
+
+
+// let num = 101103;
+// let res = "";
+// while (num > 0) {
+//     res+= num%10
+//     num = Math.floor(num/10)
+// }
+// console.log(Number(res));
+
+// let data = [23, 45, 67, 89, 12, 1];
+// let max1 = 0;
+// let max2 = 0;
+// for (let i = 0; i < data.length; i++) {
+//     if (data[i] > max1) {
+//         max1 = data[i];
+//     }
+//     if(data[i]<max1 && data[i]>max2){
+//         max2 = data[i]
+//     }
+// }
+// // for (let i = 0; i < data.length; i++) {
+
+// // }
+// console.log(max2);
+
+// let str1 = "VIKAS";
+// let str2 = "ASVKI";
+// let s1 = str1.split("").sort();
+// let s2 = str2.split("").sort();
+// let ss = s1.join("");
+// let sss = s2.join("");
+// if (ss === sss) {
+//     console.log("Given string are Anagram.");
+// }
+// else{
+//     console.log("Given string are not Anagram.");
+// }
+
+
+
+// let str1 = "VIKAS";
+// let str2 = "ASVKI";
+// for(let i = 0;i<str1.length;i++){
+//     if()
+// }
+
+// let data = [3, 10, 100, 13, 8];
+// let a = data.sort(function (a,b){return a-b})
+// console.log(a);
+
+// let num = 1234;
+// let res = null;
+// while (num > 0) {
+//     res = (num % 10)+(10*num)
+//     num = Math.floor(num/10)
+// }
+// console.log(res);
+
+// let str1 = "vikas";
+// let str2 = "akisv";
+// if(str1.length===str2.length){console.log(`Length is equal`);}
+// for(let i = 0;i<str1.length;i++){
+//     for(let j = 0;j<str2.length;j++){
+//         if(str1[i]==str2[i]){}
+//     }
+// }
+
+
+
+
