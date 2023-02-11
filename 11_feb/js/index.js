35c92d5a2f02c18b328a511034f6f6e525df783b
VikasNimap <118968201+VikasNimap@users.noreply.github.com>
2023-02-11 17:29:27 +0530

    Add files via upload

---
+++ 11_feb/js/index.js
@@ -0,0 +1,17 @@
+console.log(124);
+
+// let data = null;
+
+// let data = [
+    //     {
+        //         name:"vikas"
+        // }
+        // ]
+        // console.log(data);
+        // let arr = [];
+        // data = [{name:"vikas"},{age:20}];//,[{a:"name"}],[{b:"hello"}]]
+        // let a = [{a:"name"}];let b = [{b:"hello"}];
+        // arr.push(...data);
+        // arr.push(...a);
+        // arr.push(...b)
+        // console.log(arr);
