cf7d871928ae4ee2b8655ccf136caa562e3f9004
VikasNimap <118968201+VikasNimap@users.noreply.github.com>
2023-02-18 17:42:15 +0530

    Add files via upload

---
+++ 18_feb/js/index.js
@@ -0,0 +1,43 @@
+// console.log("hello");
+// let arr = [ 'Orange', 'Orange', 'Mango', 'Apple', 'Orange', 'Banana', 'Mango', 'Kiwi' ];
+// let newArr = [];
+// for(let i = 0;i<arr.length;i++){
+//     for(let j = 0;j<i;j++){
+//         if(arr[i]==arr[j]){
+//             newArr.push(arr[i])
+//         }
+//     }
+// }console.log(newArr);
+// let arr = [ 'Orange', 'Orange', 'Mango', 'Apple', 'Orange', 'Banana', 'Mango', 'Kiwi' ];
+// let newArr = [...new Set(arr)];
+// console.log(newArr);
+
+// let arr = [ 'Orange', 'Orange', 'Mango', 'Apple', 'Orange', 'Banana', 'Mango', 'Kiwi' ];
+// let unique = [];
+// for(let i = 0;i<arr.length;i++){
+//     if(!unique.includes(arr[i])){
+//         unique.push(arr[i])
+//     }
+// }console.log(unique);
+// let arr = [ 'Orange', 'Orange', 'Mango', 'Apple', 'Orange', 'Banana', 'Mango', 'Kiwi' ];
+// let a = arr.sort();
+// console.log(a);
+
+// let data = [23,56,89,12,34,4];
+// let min = data[0];
+// let min2 = data[1];
+// for(let i = 0;i<data.length;i++){
+//     if(data[i]<min){min=data[i]}
+// }
+// for(let i = 0;i<data.length;i++){
+//     if(data[i]>min && data[i]<min2){min2 = data[i]}
+// }
+// console.log(min2);
+
+// const {name,email,phone} = req.body
+// if(!name){
+//     console.log(`name not found`);
+// }
+// else{
+//     console.log(`name found====`);
+// }
