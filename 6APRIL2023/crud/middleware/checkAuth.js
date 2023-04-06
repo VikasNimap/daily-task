f685ce6b800d25345ab2d735b647e6eb6de0859d
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-04-06 18:00:22 +0530

    pr

--- 6APRIL2023/crud/middleware/checkAuth.js
+++ 6APRIL2023/crud/middleware/checkAuth.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 
 exports.verify = async (req, res, next) => {
     const token = req.header("Authorization").split(" ")[1];
-    const decode = jwt.verify(token,"vikas");
-    
+    const decode = jwt.verify(token, "vikas");
 
+    req.userDetails = decode;
+    console.log(req.userDetails);
 }
