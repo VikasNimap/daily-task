da30b2475aff7665860ff43b355f844d2ca4bb92
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-04-06 17:51:53 +0530

    pr

--- 6APRIL2023/crud/controllers/userController.js
+++ 6APRIL2023/crud/controllers/userController.js
@@ -1,8 +1,31 @@
 const db = require('../models/index');
+const jwt = require('jsonwebtoken');
 
-
-exports.createUser = async(req,res)=>{
+exports.createUser = async (req, res) => {
     const data = req.body;
     const response = await db.users.create(data);
     res.send(response)
+}
+
+exports.getUser = async (req, res) => {
+    const response = await db.users.findAll();
+    res.send(response)
+}
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    if (email && password) {
+        const response = await db.users.findOne({ where: { email: email } });
+        const pass = response.password;
+        if (pass) {
+            const signature = jwt.sign(response.dataValues, "vikas", { expiresIn: "1h" });
+            return res.send({result:signature})
+        }
+        else {
+            return res.send(`not valid password`)
+        }
+    }
+    else {
+        return res.send(`please provide email as well as password.`)
+    }
 }
--- 6APRIL2023/crud/routes/index.js
+++ 6APRIL2023/crud/routes/index.js
@@ -3,4 +3,6 @@ var router = express.Router();
 const controllers = require('../controllers/userController');
 /* GET home page. */
 // router.post('/users',controllers.createUser)
+router.get('/users', controllers.getUser);
+router.post('/login', controllers.login);
 module.exports = router;
