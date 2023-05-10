1040e2e0b1e06a10f696570ffffc82c04b5b7f83
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-05-10 10:40:15 +0530

    practice

---
+++ index.js
@@ -0,0 +1,32 @@
+// console.log("hello");
+
+// let data = [12, 35, 1, 10, 34, 1, 35];
+// let max1 = 0;
+// let max2 = 0;
+// for (let i = 0; i < data.length; i++) {
+//     if (data[i] > max1) {
+//         max2 = max1;
+//         max1 = data[i];
+//     } else if (data[i] > max2 && data[i] < max1) {
+//         max2 = data[i];
+//     }
+// }
+// console.log(max2);
+
+// let num = 345;
+// let res = 0;
+// while (num > 0) {
+//     res = res*10 + num%10;
+//     num = Math.floor(num/10)
+//     console.log(res);
+// }
+// console.log(res);
+
+// let str1 = 'vikas';
+// let str2 = 'vishl';
+
+// function anagram(str1, str2) {
+//     if (str1.length != str2.length) {
+//         return 
+//     }
+// }
