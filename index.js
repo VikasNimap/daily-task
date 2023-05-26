1b95fa6de7c209617dc07a3ef952b4c0ff997c8b
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-05-26 14:54:40 +0530

    try-catch

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-// console.log(1234567890);
+// // console.log(1234567890);
 
-let arr = undefined
-function test() {
-    return new Promise((resolve, reject) => {
-        if (arr) {
-            resolve(arr)
-        } else {
-            reject(new Error('arr is falsy', arr))
-        }
-    })
-}
-test().then(
-    (arr) => {
-        console.log('inside then', arr);
-    })
-    .catch(
-        (err) => {
-            console.log('inside catch', err.message);
-        })
+// let arr = undefined
+// function test() {
+//     return new Promise((resolve, reject) => {
+//         if (arr) {
+//             resolve(arr)
+//         } else {
+//             reject(new Error('arr is falsy', arr))
+//         }
+//     })
+// }
+// test().then(
+//     (arr) => {
+//         console.log('inside then', arr);
+//     })
+//     .catch(
+//         (err) => {
+//             console.log('inside catch', err.message);
+//         })
+
+// let a = 'vikas';
+// try {
+//     if (a) {
+//         console.log('inside if block', a);
+//         throw Error('ERROR')
+//     }
+//     else {
+//         console.log('inside else block ', a);
+//     }
+// } catch (error) {
+//     console.log('inside error', error);
+// }
