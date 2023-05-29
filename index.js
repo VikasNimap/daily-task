b0648d571edd6b778c7b4b84a1dde4c9380320bb
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-05-29 09:36:30 +0530

    practice

---
+++ index.js
@@ -0,0 +1,57 @@
+// console.log(123);
+// let str = 'Engineer';
+// function freq() {
+//     let word = str.toLowerCase();
+//     let query = {};
+//     for (let char of word) {
+//         if (query[char]) {
+//             query[char]++
+//         } else {
+//             query[char] = 1
+//         }
+//     }
+//     let res = '';
+//     for (let ch in query) {
+//         res += ch + query[ch]
+//     }
+//     return res
+// }
+// console.log(freq())
+
+// let arr = ['cat', 'act', 'pat', 'tap', 'eat', 'tac'];
+// function anagram(arr) {
+//     let query = {};
+//     for (let i = 0; i < arr.length; i++) {
+//         let word = arr[i].split('').sort().join('');
+//         if (query[word]) {
+//             query[word].push(arr[i])
+//         } else {
+//             query[word] = [];
+//             query[word].push(arr[i])
+//         }
+//     }
+//     return Object.values(query)
+// }
+// console.log(anagram(arr))
+
+// let getData = async (req, res) => {
+//     return new Promise((resolve, reject) => {
+//         reject(1)
+//     }).then(() => {
+//         return ('hello inside then');
+//     }).catch((err) => {
+//         return ({ error: true, message: 'in catch error', err })
+//     })
+// }
+
+// let findData = async (req, res) => {
+//     let result = await getData();
+//     console.log(result)
+// }
+// findData()
+
+// let arr = ['vikas', 'shiva', 'akash', 'aditya'];
+// let res = arr.splice(0, 3);
+// let res = arr.slice(2);
+// console.log(res);
+// console.log(arr);
