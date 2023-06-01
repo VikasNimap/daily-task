a9a05d362670b8e00580b4de991bf55a7f0b84ab
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-06-01 12:16:21 +0530

    function & type-aliases

--- test.js
+++ test.js
@@ -12,22 +12,50 @@
 //     }
 // }
 // console.log(multiplyNums("akash", 7));
-function printAll(a) {
-    if (typeof a === "string") {
-        for (var i = 0; i < a.length; i++) {
-            console.log(a[i]);
-        }
-    }
-    else if (typeof a === "object") {
-        if (Array.isArray(a)) {
-            for (var _i = 0, a_1 = a; _i < a_1.length; _i++) {
-                var str = a_1[_i];
-                return a;
-            }
-        }
-    }
-    else {
-        return "Argument is: ".concat(a);
-    }
+// function printAll(a: string | string[] | object) {
+//     if (typeof a === "string") {
+//         for (let i = 0; i < a.length; i++) {
+//             console.log(a[i]);
+//         }
+//     } else if (typeof a === "object") {
+//         if (Array.isArray(a)) {
+//             for (let str of a) {
+//                 return a
+//             }
+//         }
+//     }   else{
+//         return `Argument is: ${a}`
+//     }
+// }
+// console.log(printAll(undefined))
+// function getOnlineUsers(num: number|string) {
+//     if(num){
+//         return `Total online users are ${num}`
+//     }   else{
+//         return `no one here...`
+//     }
+// }
+// console.log(getOnlineUsers("5"));
+// enum Role {
+//     ADMIN, EDITOR,USER
+// };
+// console.log(Role.USER)
+// let a: any = ['vikas', 12, 'vguhb'];
+// console.log(a);
+// function combine(a: string | number, b: string | number) {
+//     if (typeof a === "string" && typeof b === "string"){
+//         return a + b
+//     }   else{
+//         return a+b
+//     }
+// }
+// console.log(combine(5,4));
+//type-alias
+// type dataType = string | number | boolean | undefined;
+// let a: dataType = 300;
+// let b: dataType = "300";
+// let c: dataType = true;
+function test() {
+    return 200;
 }
-console.log(printAll("undefined"));
+console.log(test());
--- test.ts
+++ test.ts
@@ -18,20 +18,61 @@
 
 // console.log(multiplyNums("akash", 7));
 
-function printAll(a: string | string[] | object) {
-    if (typeof a === "string") {
-        for (let i = 0; i < a.length; i++) {
-            console.log(a[i]);
-        }
-    } else if (typeof a === "object") {
-        if (Array.isArray(a)) {
-            for (let str of a) {
-                return a
-            }
-        }
-    }   else{
-        return `Argument is: ${a}`
-    }
-}
+// function printAll(a: string | string[] | object) {
+//     if (typeof a === "string") {
+//         for (let i = 0; i < a.length; i++) {
+//             console.log(a[i]);
+//         }
+//     } else if (typeof a === "object") {
+//         if (Array.isArray(a)) {
+//             for (let str of a) {
+//                 return a
+//             }
+//         }
+//     }   else{
+//         return `Argument is: ${a}`
+//     }
+// }
 
-console.log(printAll("undefined"))
+// console.log(printAll(undefined))
+
+
+// function getOnlineUsers(num: number|string) {
+//     if(num){
+//         return `Total online users are ${num}`
+//     }   else{
+//         return `no one here...`
+//     }
+// }
+
+// console.log(getOnlineUsers("5"));
+
+
+// enum Role {
+//     ADMIN, EDITOR,USER
+// };
+// console.log(Role.USER)
+
+// let a: any = ['vikas', 12, 'vguhb'];
+// console.log(a);
+
+// function combine(a: string | number, b: string | number) {
+//     if (typeof a === "string" && typeof b === "string"){
+//         return a + b
+//     }   else{
+//         return a+b
+//     }
+// }
+// console.log(combine(5,4));
+
+//type-alias
+
+// type dataType = string | number | boolean | undefined;
+// let a: dataType = 300;
+// let b: dataType = "300";
+// let c: dataType = true;
+
+function test(): number {
+    return 200;
+}
+console.log(test());
