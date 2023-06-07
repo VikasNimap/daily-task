8666cc668e8c205cea6a8611e6de75e5227dbade
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-06-07 17:34:35 +0530

    class-practice

--- home.js
+++ home.js
@@ -80,7 +80,7 @@
 //     return new Promise((res, rej) => {
 //         res(console.log('hii'))
 // })
-    
+
 // }
 //  function temp() {
 //      test()
@@ -93,4 +93,66 @@
 // temp()
 // testing()
 // console.log('outaide fn');
+
+// let arr = ['abc', '123', 34, 56];
+// for (let ele in arr) {
+//     console.log(arr[ele]);
+// }
+
+// let arr = [1, 45, 67, 3, 2];
+// let position;
+// let ele = 67;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === ele) {
+//         position = i
+//     }
+// }
+// for (let i = position; i < arr.length; i++) {
+//         arr[i] = arr[i + 1]
+// }
+// arr.length=arr.length-1
+// console.log(arr);
+
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [10, 20, 30, 40, 50];
+// let arr3 = [];
+// for (let i = 0; i < arr1.length; i++) {
+//     arr3[i] = arr1[i]
+// }
+// for (let i = 0; i < arr2.length; i++) {
+//     arr3[arr1.length + i] = arr2[i]
+// }
+// console.log(arr3);
+
+
+
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [10, 20, 30, 40, 50];
+// let arr3 = [];
+// let a1 = 0;
+// let a2 = 0;
+// let a3 = 0;
+// while () {
+//     if (arr1[a1] < arr2[a2]) {
+//         arr3[d3] = arr1[a1]
+//         a3++
+//         a1++
+//     } else {
+//         arr3[d3] = arr2[a2]
+//         a3++
+//         a2++
+//     }
+// }
+
+// let details = {
+//     name: 'vikas',
+//     email: 'vikas@gmail.com',
+//     adderss: {
+//         home: [/*'new York', 'Mumbai', 'uttar Pradesh'*/],
+//         work: ['Bangalore', 'Madhya Pradesh']
+//     }
+// }
+
+// console.log(details?.adderss?.home?.length?.pune);
+
 
--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
+type Employee = {
+    name: string, email: string, phone?: string, password: string
+}
+
 // let details: Employee = {
 //     name: 'vikas maurya',
 //     email: 'vikas@gmail.com',
 //     // phone: '1234567890',
 //     password: '12345'
 // };
+
 // console.log(details);
+
+
 // let details: Employee = {
 //     name: 'vikas maurya',
 //     email: 'vikas@gmail.com',
@@ -12,7 +19,9 @@
 //     password: '12345',
 //     age: '21'
 // };
+
 // console.log(details);
+
 // function cmToInch(height: number | string) {
 //     if (typeof height === 'number') {
 //         return Math.round(height / 2.54 / 12) + ' ft'
@@ -21,6 +30,8 @@
 //     }
 // }
 // console.log(cmToInch(164.592));
+
+
 // function cmToInch(height: number | string) {
 //     if (typeof height === 'number') {
 //         return Math.round(height / 2.54 / 12) + ' ft'
@@ -29,29 +40,43 @@
 //     }
 // }
 // console.log(cmToInch('164.592'));
+
+
+
 // type Draggable = {
 //     drag: () => void
 // };
 // type Resizable = {
 //     resize: () => void
 // }
+
 // type UiWidget = Draggable & Resizable
+
 // let textBox: UiWidget = {
 //     drag: () => { },
 //     resize: () => { }
 // }
 // type Quantity = 50 | 100
 // let quantity: Quantity = 100;
+
+
 // function greet(name: string | undefined) {
 //     console.log(`Hii ${name?.toUpperCase()}`);
+
 // }
 // greet('undefined')
+
+
 // type Customer = { birthday?: Date };
 // function getCustomer(id: number): Customer | null | undefined {
 //     return id === 0 ? null : { birthday: new Date() }
 // }
+
 // let customer = getCustomer(12);
 // console.log(customer?.birthday?.getDay(), customer?.birthday?.getDate(), customer?.birthday?.getMonth(), customer?.birthday?.getFullYear());
+
+
+
 // let details = {
 //     name: 'vikas',
 //     email: 'vikas@gmail.com',
@@ -60,32 +85,47 @@
 //         work: ['Bangalore', 'Madhya Pradesh']
 //     }
 // }
+
 // console.log(details?.adderss?);
+
 // let log: any = null;
 // console.log(log?.('a'))
+
 // let speed: number | null = null;
+
 // let ride = {
 //     speed: speed !== null?? 30
 // }
+
 // console.log(ride);
+
+
 // let phone = <HTMLInputElement>document.getElementById('phone');
 // phone.value
+
 // function render(document: unknown) {
+
 //     document.move();
 // }
-var Account = /** @class */ (function () {
-    function Account(id, owner, balance) {
+
+class Account {
+    id: number;
+    owner: string;
+    balance: number;
+
+    constructor(id: number, owner: string, balance: number) {
         this.id = id;
         this.owner = owner;
         this.balance = balance;
     }
-    Account.prototype.deposit = function (amount) {
+
+    deposit(amount: number): void {
         if (amount <= 0)
-            throw new Error('Invalid Amount');
-        this.balance += amount;
-    };
-    return Account;
-}());
-var account = new Account(67313170674, 'Mr. Vikas Maurya', 4000);
-account.deposit(1000);
+            throw new Error('Invalid Amount')
+        this.balance += amount
+    }
+}
+
+let account = new Account(67313170674, 'Mr. Vikas Maurya', 4000);
+account.deposit(1000)
 console.log(typeof account);
