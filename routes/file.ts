bcc4640cc6899d764a92b1d16d58f48d87b3b523
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-06-16 11:44:38 +0530

    practice

--- routes/file.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-// import { Router } from "express-serve-static-core";
-import { file } from '../controller/fileController';
-const route = Router();
-route.post('/', file);
-
-export { route }
