c0571b35e0a59ffa787612f95e8d498a2ff9be58
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-06-24 09:10:26 +0530

    practice

---
+++ index.js
@@ -0,0 +1,20 @@
+// console.log(12345);
+
+// let num = 123;
+// let res = String(num).split("").reverse().join("");
+// let r1 = Number(res);
+// console.log(r1);
+
+// let num = 123;
+// let res = 0;
+// while (num > 0) {
+//     res = res * 10 + (num % 10);
+//     num = Math.floor(num / 10);
+// }
+// console.log(res);
+
+// function test() {
+//     console.log(typeof c);
+// }
+// test();
+
