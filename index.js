e5f85da142777b8f9320288520849604a0116d3d
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-06-26 09:19:28 +0530

    practice

---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log(12345);
+
+// console.log(
+//     'vikas' + {}
+// );
+
+
+// let arr = [12, 34, 56, 78, 90, 12, 34, 56, 78, 90, 12, 34, 56, 8, 12, 12, 12, 1, 2, 12, 78, 34];
+// let res = arr.filter((ele, i) => {
+//     return arr.indexOf(ele) != arr.lastIndexOf(ele) && arr.lastIndexOf(ele) == i
+// })
+// console.log(res);
+
