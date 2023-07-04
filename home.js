b9d5dcd814da9fd3d9a12aefd467fb16d24f40f3
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-07-04 16:19:12 +0530

    event-emitter-practice

--- home.js
+++ home.js
@@ -47,22 +47,56 @@
 // var message = 20;
 // console.log(global)
 
-const EventEmitter = require('events');
-const eventEmitter = new EventEmitter();
+// const EventEmitter = require('events');
+// const eventEmitter = new EventEmitter();
 
-class Logger extends EventEmitter {
-    log(message) {
-        console.log(message);
-        this.emit('messageLogged', { id: 1, name: "vikas" })
-    }
-}
+// class Logger extends EventEmitter {
+//     log(message) {
+//         console.log(message);
+//         this.emit('messageLogged', { id: 1, name: "vikas" })
+//     }
+// }
 
-eventEmitter.on('click', function (data) {
-    console.log('Event emitter is logged', data);
-})
+// eventEmitter.on('click', function (data) {
+//     console.log('Event emitter is logged', data);
+// })
 
-eventEmitter.on('click', function (data) {
-    console.log('Event emitter is logged twice', data);
-})
+// eventEmitter.on('click', function (data) {
+//     console.log('Event emitter is logged twice', data);
+// })
+
+
+// let arr = ['Apple', 'Mango', 'Banana', 'Pomegranate'];
+// let a = ['Peru', 'Pear']
+// let res = arr.splice(1, 1, a);
+// // let res = arr.slice(0, -1);
+// console.log(arr);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+// // fruits.splice(2, 2);
+// // console.log(fruits);
+
+// const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+// let index = 2;
+// let fruit = fruits.concat(fruits1);
+// console.log(console.log(console.log()));
+
+
+// const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+// let fruit = fruits1.splice(0,2);
+// console.log(fruits1);
+
+// console.log(eval('1+3*5'));
+
+
+
+
+
+
+
+
+
+
+
 
 
