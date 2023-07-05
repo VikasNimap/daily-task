592d721d74ab95efb529500f7efbd743eb77a320
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-07-05 12:14:22 +0530

    TS-server-setup

---
+++ index.js
@@ -0,0 +1,70 @@
+// console.log(1234);
+
+// let data = [1, 2, 3, 4, 5, 6];
+// data = [];
+// console.log(data);
+
+// let data = [1, 2, 3, 4, 5, 6];
+// for (let i = data.length; i >= 0; i--) {
+//     data.pop()
+// }
+// console.log(data);
+
+// let data = [1, 2, 3, 4, 5, 6];
+// while(data.length>0){
+//     data.pop()
+// }
+// console.log(data);
+
+
+// let data = [1, 2, 3, 4, 5, 6];
+
+// let res = data.splice(0, data.length);
+// console.log(data);
+
+// function test() {
+//     try {
+//         let a = 100;
+//         let b = a / 0;
+//         return 1
+//     } catch (error) {
+//         return 2
+//     } finally {
+//         return 4
+//     }
+// }
+
+// console.log(test());
+
+// cons t eventti 
+
+// emiite.on()
+// emit
+// try {
+//     throw new Error('Err')
+// } catch (error) {
+//     var error = 10, y = 20
+//     console.log(error);
+// }
+// console.log(error);
+// console.log(y);
+
+// console.log(error);
+
+// const EventEmitter = require('events');
+// let events = new EventEmitter();
+
+
+// events.emit('abc');
+// events.on('abc', function () {
+//     console.log('hello');
+// })
+// events.emit('abc');
+
+// let data = [1, 2, 3, 4, 5, 6];
+// let a = data.slice(0,1)
+// console.log(a, data);
+
+
+// (valueOf(1))
+
