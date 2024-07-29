c6665db7453761c74f080e676d01d4460f33582e
vikasqx <vikas@qxlabai.com>
2024-07-29 14:19:00 +0530

    making user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('user')
-export class UserController {}
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+@Controller('/api/user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Get()
+  getHello(): string {
+    return this.userService.getHello();
+  }
+}
